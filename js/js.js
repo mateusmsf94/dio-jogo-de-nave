a35c6c9649bfec3ea0734b60f7bd8861ded1dbd0
mateusmsf94 <mateusmelosf@gmail.com>
2022-02-15 18:28:41 -0300

    parte 19 completa

--- js/js.js
+++ js/js.js
@@ -208,6 +208,7 @@ function start() { // Inicio da funcao start()
         if (colisao3.length > 0) {
 
             pontos = pontos + 100;
+            velocidade=velocidade+0.3;
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
 
